66816ff50f371d608a555408d14ec9b644ac5702
Régis BOUGAUD <r.bougaud@ressources-consultants.fr>
2025-05-04 10:45:55 +0200

    Preparation Structure du projet

---
+++ PoleEmploiTestApi/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        return services;
+    }
+}
